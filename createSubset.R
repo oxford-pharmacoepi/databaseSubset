b3e84754a5c310e9f0521381a9a310199e3dfaec
catalamarti <marticatalasabate@gmail.com>
2023-04-12 09:12:33 +0100

    initial version

---
+++ createSubset.R
@@ -0,0 +1,88 @@
+library(CDMConnector)
+library(DBI)
+library(log4r)
+library(dplyr)
+library(dbplyr)
+library(here)
+library(IncidencePrevalence)
+library(readr)
+library(tidyr)
+
+db_name <- "aurum"
+number_individuals <- 100000
+
+
+server_dbi <- switch(
+  db_name,
+  "gold" = "cdm_gold_202201",
+  "aurum" = "cdm_aurum_p21_000557",
+  "subsetaurum" = "p20_059_cdm_aurum",
+  "pharmetrics" = "cdm_iqvia_pharmetrics_plus_202203"
+)
+
+user <- Sys.getenv("DB_USER")
+password <- Sys.getenv("DB_PASSWORD")
+port <- Sys.getenv("DB_PORT")
+host <- Sys.getenv("DB_HOST")
+dbms <- "postgresql"
+db <- dbConnect(
+  RPostgres::Postgres(),
+  dbname = server_dbi,
+  port = port,
+  host = host,
+  user = user,
+  password = password
+)
+
+# The name of the schema that contains the OMOP CDM with patient-level data
+cdm_database_schema <- "public"
+
+# The name of the schema where results tables will be created 
+results_database_schema <- "public_100k"
+
+# minimum counts that can be displayed according to data governance
+minimum_counts <- 5
+
+# create cdm reference ----
+cdm <- CDMConnector::cdm_from_con(
+  con = db,
+  cdm_schema = cdm_database_schema,
+  write_schema = results_database_schema,
+  cdm_name = db_name
+)
+
+subsetPerson <- cdm$person %>%
+  dplyr::select("person_id") %>%
+  dplyr::mutate(rand = dbplyr::sql("random()")) %>%
+  dplyr::arrange(.data$rand) %>%
+  compute()
+
+subsetPerson <- subsetPerson %>%
+  head(number_individuals) %>%
+  compute()
+
+subsetPerson <- subsetPerson %>%
+  dplyr::arrange() %>% 
+  dplyr::select("person_id") %>%
+  compute()
+
+for (nam in names(cdm)) {
+  print(nam)
+  tictoc::tic()
+  if ("person_id" %in% colnames(cdm[[nam]])) {
+    xx <- cdm[[nam]] %>%
+      dplyr::inner_join(subsetPerson, by = "person_id") %>%
+      computeQuery(nam, FALSE, "public_100k", TRUE)
+  } else {
+    xx <- cdm[[nam]] %>%
+      computeQuery(nam, FALSE, "public_100k", TRUE)
+  }
+  tictoc::toc()
+}
+
+cdm <- CDMConnector::cdm_from_con(
+  con = db,
+  cdm_schema = "public_100k",
+  write_schema = "results",
+  cdm_name = db_name
+)
